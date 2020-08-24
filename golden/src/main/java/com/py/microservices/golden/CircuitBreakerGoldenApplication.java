c0a2237b242431706d8c9886a9f33144528d8c78
Mirta Gonzalez <mgonzalez@sodep.com.py>
2020-08-23 23:12:08 -0400

    Renombra paquete de Golden

--- golden/src/main/java/com/py/microservices/golden/CircuitBreakerGoldenApplication.java
+++ golden/src/main/java/com/py/microservices/golden/CircuitBreakerGoldenApplication.java
@@ -1,4 +1,4 @@
-package com.example.golden;
+package com.py.microservices.golden;
 
 import com.netflix.appinfo.ApplicationInfoManager;
 import com.netflix.discovery.EurekaClient;
@@ -6,22 +6,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @EnableCircuitBreaker
 @RestController
+@SpringBootApplication
 @EnableDiscoveryClient
 @EnableEurekaClient
-@SpringBootApplication
 public class CircuitBreakerGoldenApplication {
-	@Autowired
-	@Lazy
-	private EurekaClient eurekaClient;
+
+	@Value("${spring.application.name}")
+	private String appName;
+
+	@Bean
+	public RestTemplate rest(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 
 	@RequestMapping(value = "/alo")
 	public String helloWorld() {
--- golden/src/test/java/com/py/microservices/golden/GoldenApplicationTests.java
+++ golden/src/test/java/com/py/microservices/golden/GoldenApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.golden;
+package com.py.microservices.golden;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

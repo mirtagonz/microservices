15a31afe1b24ec168ecb350e4168ebc97b7f3651
Mirta Gonzalez <mgonzalez@sodep.com.py>
2020-08-23 23:16:23 -0400

    Agrega a kangaroo al build

--- build-jars.sh
+++ build-jars.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-PROJECTS=" golden arquimedes gandalf knowsmore"
+PROJECTS=" golden arquimedes gandalf knowsmore kangaroo"
 WORKING_DIR=$PWD
 for PRO in $PROJECTS; do
 	cd $PRO
 	echo "Building $PRO"
 	sleep 1
-	mvn -DskipTests clean package
+	mvn -DskipTests clean install
 	cd $WORKING_DIR
 done
 

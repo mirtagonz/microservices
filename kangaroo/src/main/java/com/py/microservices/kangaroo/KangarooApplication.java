b23ddc7a359a35fa347086a41b5299b4efe97ac3
Mirta Gonzalez <mgonzalez@sodep.com.py>
2020-08-24 23:05:26 -0400

    Renombra clases

--- kangaroo/src/main/java/com/py/microservices/kangaroo/KangarooApplication.java
+++ kangaroo/src/main/java/com/py/microservices/kangaroo/KangarooApplication.java
@@ -4,21 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableEurekaClient
-public class CircuitBreakerKangarooApplication {
-
-    @RequestMapping(value = "/ping")
-    public String readingList() {
-        return "Kangaroo response";
-    }
-
+public class KangarooApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CircuitBreakerKangarooApplication.class, args);
+        SpringApplication.run(KangarooApplication.class, args);
     }
 }
